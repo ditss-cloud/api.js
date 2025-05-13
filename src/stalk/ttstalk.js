4cd973efb8b8f2d1cd6322ae0e9a8d00b3047887
adiett <paadit041@gmail.com>
2025-05-13 14:29:49 +0700

    Update ttstalk.js

--- src/stalk/ttstalk.js
+++ src/stalk/ttstalk.js
@@ -1,5 +1,5 @@
 const cheerio = require('cheerio');
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
 async function tiktokStalk(user) {
   try {
@@ -13,39 +13,41 @@ async function tiktokStalk(user) {
     const data = $('#__UNIVERSAL_DATA_FOR_REHYDRATION__').text();
     const result = JSON.parse(data);
     if (result['__DEFAULT_SCOPE__']['webapp.user-detail'].statusCode !== 0) {
-      const ress = {
+      return {
         status: 'error',
         message: 'User not found!',
       };
-      console.log(ress);
-      return ress;
     }
-    const res = result['__DEFAULT_SCOPE__']['webapp.user-detail']['userInfo'];
-    return res;
+    return result['__DEFAULT_SCOPE__']['webapp.user-detail']['userInfo'];
   } catch (err) {
-    console.log(err);
-    return String(err);
+    return { status: 'error', message: err.message };
   }
 }
 
-module.exports = function(app) {
-app.get('/stalk/tiktok', async (req, res) => {
-const { apikey } = req.query;
-if (!global.apikey.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' })
-const { user } = req.query;
-if (!user) return res.json({ status: false, error: 'User is required' })
-        try {
-            let anu = await tiktokStalk(user);  
-            anu.user.heart = anu.stats.heart
-            anu.user.followerCount = anu.stats.followerCount
-            anu.user.followingCount = anu.stats.followingCount
-            res.status(200).json({
-                status: true,
-                result: anu.user
-            });
-        } catch (error) {
-            res.status(500).send(`Error: ${error.message}`);
-        }
-});
+module.exports = function (app) {
+  app.get('/stalk/tiktok', async (req, res) => {
+    const { apikey, user } = req.query;
+    if (!global.apikey.includes(apikey))
+      return res.json({ status: false, error: 'Apikey invalid' });
+    if (!user)
+      return res.json({ status: false, error: 'User is required' });
 
+    try {
+      const anu = await tiktokStalk(user);
+      if (anu.status === 'error') return res.json(anu);
+
+      // Tambahkan statistik dari stats ke user
+      anu.user.heart = anu.stats.heart;
+      anu.user.followerCount = anu.stats.followerCount;
+      anu.user.followingCount = anu.stats.followingCount;
+      anu.user.videoCount = anu.stats.videoCount; // <--- Jumlah postingan
+
+      res.status(200).json({
+        status: true,
+        result: anu.user
+      });
+    } catch (error) {
+      res.status(500).send(`Error: ${error.message}`);
+    }
+  });
 };

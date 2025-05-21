7cedd3e689e8bed513469addb11fab2770dd776b
adiett <paadit041@gmail.com>
2025-05-21 20:54:42 +0700

    Update cuci-mantap.js

--- src/random/cuci-mantap.js
+++ src/random/cuci-mantap.js
@@ -1,24 +1,42 @@
+const axios = require('axios');
+
+
+const getBuffer = async (url) => {
+    const res = await axios.get(url, { responseType: 'arraybuffer' });
+    return res.data;
+};
+
+
 async function anim() {
     try {
-        const data = `https://img12.pixhost.to/images/507/570627648_skyzopedia.jpg`;
-        const response = await getBuffer(data);
+        const urls = [
+            'https://img12.pixhost.to/images/507/570627648_skyzopedia.jpg',
+            'https://img12.pixhost.to/images/507/570627649_example1.jpg',
+            'https://img12.pixhost.to/images/507/570627650_example2.jpg',
+            'https://img12.pixhost.to/images/507/570627651_example3.jpg'
+            // Tambahkan URL lain sesukamu
+        ];
+        const randomUrl = urls[Math.floor(Math.random() * urls.length)];
+        const response = await getBuffer(randomUrl);
         return response;
     } catch (error) {
         throw error;
     }
 }
 
+// Export router
 module.exports = function app(app) {
     app.get('/random/papayang', async (req, res) => {
         try {
-            const pedo = await anim();
+            
+            const buffer = await anim();
             res.writeHead(200, {
                 'Content-Type': 'image/png',
-                'Content-Length': pedo.length,
+                'Content-Length': buffer.length,
             });
-            res.end(pedo);
+            res.end(buffer);
         } catch (error) {
             res.status(500).send(`Error: ${error.message}`);
         }
     });
-}
+};

d6e45cf279360db723334f7d0bc726d561a6b1c5
ditss-cloud <ditss.cloud@gmail.com>
2025-06-02 00:48:43 +0700

    Update happymod.js

--- src/search/happymod.js
+++ src/search/happymod.js
@@ -2,40 +2,55 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 async function happymod(query) {
-  try {
-    const res = await axios.get("https://unduh.happymod.com/search.html?q=" + query);
-    const html = res.data;
-    const $ = cheerio.load(html);
-    const data = [];
+  return new Promise((resolve, reject) => {
+    axios.get("https://www.happymod.com/search.html?q=" + query)
+      .then(({ data }) => {
+        const $ = cheerio.load(data);
+        const nama = [];
+        const link = [];
+        const rating = [];
+        const thumb = [];
+        const version = [];
+        const format = [];
 
-    $('article.flex-item').each((index, element) => {
-      const name = $(element).find('h2.has-normal-font-size.no-margin.no-padding.truncate').text().trim();
-      const version = $(element).find('div.has-small-font-size.truncate').first().text().trim();
-      const url = $(element).find('a.app.clickable').attr('href');
-      const thumb = $(element).find('img').attr('data-src') || $(element).find('img').attr('src');
-      const rating = $(element).find('div.star-box').text().trim().replace(/\s+/g, ' ') || "No rating";
-
-      if (name && version && url) {
-        data.push({
-          name,
-          version,
-          rating,
-          thumb,
-          url: "https://unduh.happymod.com/" + url
+        // Judul + link
+        $('div.pdt-app-box h3 > a').each((i, el) => {
+          nama.push($(el).text().trim());
+          link.push('https://happymod.com' + $(el).attr('href'));
         });
-      }
-    });
 
-    return {
-      status: true,
-      data
-    };
-  } catch (error) {
-    return {
-      status: false,
-      message: "permintaan tidak dapat diproses!!"
-    };
-  }
+        // Rating
+        $('div.pdt-app-box div.stars > span').each((i, el) => {
+          rating.push($(el).text().trim());
+        });
+
+        // Thumbnail
+        $('div.pdt-app-box a > img').each((i, el) => {
+          thumb.push($(el).attr('data-original'));
+        });
+
+        // Version
+        $('div.pdt-version').each((i, el) => {
+          version.push($(el).text().replace('Latest version: ', '').trim());
+        });
+
+        for (let i = 0; i < link.length; i++) {
+          format.push({
+            judul: nama[i],
+            version: version[i] || 'Unknown',
+            thumb: thumb[i] || null,
+            rating: rating[i] || 'N/A',
+            link: link[i]
+          });
+        }
+
+        resolve({
+          creator: 'ditss',
+          data: format
+        });
+      })
+      .catch(reject);
+  });
 }
 
 module.exports = function (app) {
@@ -48,7 +63,7 @@ module.exports = function (app) {
       const results = await happymod(q);
       res.status(200).json({
         status: true,
-        result: results.data
+        result: results
       });
     } catch (error) {
       res.status(500).send(`Error: ${error.message}`);
